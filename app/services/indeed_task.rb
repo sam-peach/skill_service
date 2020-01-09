5a87f9337bb29991e92338b2bc5577cf1a652620
Sam Peach <sampeachart@gmail.com>
2020-01-09 07:20:11 -0500

    Debugging

--- app/services/indeed_task.rb
+++ app/services/indeed_task.rb
@@ -1,14 +1,14 @@
-class IndeedTask
-  include Delayed::RecurringJob
-  run_at '8:00am' 
-  run_at '1:00pm'
-  run_at '5:00pm'
-  timezone 'US/Eastern'
-  def perform
-    ScraperProcessor.delay.process(site: "indeed")
-  end
+# class IndeedTask
+#   include Delayed::RecurringJob
+#   run_at '8:00am' 
+#   run_at '1:00pm'
+#   run_at '5:00pm'
+#   timezone 'US/Eastern'
+#   def perform
+#     ScraperProcessor.delay.process(site: "indeed")
+#   end
 
-  def max_attempts
-    1
-  end
-end
+#   def max_attempts
+#     1
+#   end
+# end
